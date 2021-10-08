21a6be78f4f81335311c0dc3134844c80ad1d2c2
skuznetsov <skuznetsov@at-consulting.ru>
2021-10-08 09:16:21 +0300

    @feature добавлены новые миграции + наработки для выполнения следующих заданий.

--- core/src/main/java/ru/digitalleague/core/controller/OrderController.java
+++ core/src/main/java/ru/digitalleague/core/controller/OrderController.java
@@ -1,5 +1,6 @@
 package ru.digitalleague.core.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,7 @@ public class OrderController {
     private TaxiService taxiService;
 
     @PostMapping("/order-taxi")
+    @ApiOperation(value = "Контроллер для заказа такси")
     public ResponseEntity<String> receive(@RequestBody OrderDetails orderDetails) {
         log.info("Received message from postman" + orderDetails);
 
--- taxi_company/src/main/java/ru/digitalleague/taxi_company/model/TaxiDriverInfoModel.java
+++ taxi_company/src/main/java/ru/digitalleague/taxi_company/model/TaxiDriverInfoModel.java
@@ -7,6 +7,9 @@ import java.time.OffsetDateTime;
 @Data
 public class TaxiDriverInfoModel {
 
+    /**
+     * Идентификатор водителя.
+     */
     private Long driverId;
 
     /**
